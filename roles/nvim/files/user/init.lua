ccbc61afbf01b1badac143852725e32435762a37
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2022-10-29 13:57:12 +0700

    .

--- roles/nvim/files/user/init.lua
+++ roles/nvim/files/user/init.lua
@@ -189,8 +189,7 @@ local config = {
                         -- quick save
                         -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
                         -- Hop mapping
-                        ["<leader>H"] = { "<cmd>nohl<cr>", desc = "Clear Highlight" },
-                        ["<leader>h"] = { "<cmd>HopWord<cr>", desc = "Hop" },
+                        ["<leader>j"] = { "<cmd>HopWord<cr>", desc = "Hop" },
                 },
                 t = {
                         -- setting a mapping to false will disable it
@@ -208,7 +207,7 @@ local config = {
                                 branch = 'v2', -- optional but strongly recommended
                                 config = function()
                                         -- you can configure Hop the way you like here; see :h hop-config
-                                        require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+                                        require 'hop'.setup { keys = 'crumwersdfjkluio' }
                                 end }
                         -- {
                         --         "Pocco81/auto-save.nvim",
